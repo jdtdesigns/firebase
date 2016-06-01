9d842ee0f6c58be6bf341e6688bf47fa3882fee8
JD <jt7903@gmail.com>
2016-05-31 20:47:50 -0400

    initial

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDghPfWq_64d3yC8zgfW1cOBVFUYb-z3iY",
+    authDomain: "test-aa658.firebaseapp.com",
+    databaseURL: "https://test-aa658.firebaseio.com",
+    storageBucket: "test-aa658.appspot.com",
+  };
+  firebase.initializeApp(config);
